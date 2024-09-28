49a78101c85710126a1fde7edbc0e8e993c30764
Pitrified <nobilipietro@gmail.com>
2024-09-28 15:46:33 +0200

    Add idea for better root_fol identification

--- src/trip_me_up/config/constants.py
+++ src/trip_me_up/config/constants.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
 # Path to the root of the project
+# MAYBE importing trip_me_up and using trip_me_up.__file__ is better
+#       as it is not related to the config file structure
 ROOT_FOL = Path(__file__).parents[3]
 
 # data folders
 STATIC_FOL = ROOT_FOL / "static"
 DATA_FOL = ROOT_FOL / "data"
+VECTORSTORE_FOL = DATA_FOL / "vectorstore"
 
 # cache folders
 CACHE_FOL = ROOT_FOL / "cache"
